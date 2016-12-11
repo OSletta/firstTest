fc801a8b5004327e2923aa2a1c8cf3d8f9dab638
eier <eier@OVEPC>
2016-12-11 06:47:46 +0100

    Started to enter user interface, vector container used to hold objects.

--- TestGit/Main.cpp
+++ TestGit/Main.cpp
@@ -1,9 +1,27 @@
 #include"Vect2D.h"
 #include"Vect3D.h"
+#include<vector> //Container for objects
 
 
 /*To add/subtract two vector, use operator +/-.
 To get dot product use operator */
+
+std::vector<oss::Vect2D> make2Dvectors()
+{
+    int numberOfVectors;
+    oss::Vect2D newObjects;
+    std::vector<oss::Vect2D> container;
+    std::cout << "Enter numbers of vectors to add in database:";
+    std::cin >> numberOfVectors;
+
+    for (int n = 0; n < numberOfVectors; n++)
+        container.push_back(newObjects);
+
+    return container;
+
+
+}
+
 
 int main()
 {
